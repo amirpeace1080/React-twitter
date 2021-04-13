d37e494f7b55200cedcb74a96aab853b5d381fe0
Mohammad Sharifi <amirpeace1080@gmail.com>
2021-04-13 18:24:11 +0430

    end: movie34

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Layout = () =>{
+    return(
+        <div>
+            <div>Right Sidebar</div>
+            <div>Center Sidebar</div>
+            <div>Left Sidebar</div>
+        </div>
+    )
+}
+
+export default Layout
